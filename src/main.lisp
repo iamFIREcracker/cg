f519fac8bb597bf1cff9432b24ced387eb811e4b
Matteo Landi <matteo@matteolandi.net>
2019-05-05 16:43:30 +0200

    Add more generators

--- src/main.lisp
+++ src/main.lisp
@@ -12,10 +12,22 @@
      ',name))
 
 (define-extractor git-b-git-checkout
-    ("\\*?\\s*([^ ]*)\\s*[a-f0-9]{7,9}\\s" (branch)) ;; XXX can this be compiled?
+    ("^\\*?\\s+([^ ]+)\\s+[a-f0-9]{7,9}\\s+\\w" (branch)) ;; XXX can this be compiled?
   (format NIL "git checkout '~a'" branch))
 
-(defun extract (line &optional (extractors *extractors*))
+(define-extractor ports-kill
+    ("^(\\w+)\\s+([0-9]+)\\s+(\\w+)\\s+(\\w+)" (command pid user fd))
+  (format NIL "kill '~a' # FD ~a locked by ~a (~a)" pid fd command user))
+
+(define-extractor psg-kill
+    ("^([0-9]+)\\s+(.+)" (pid command))
+  (format NIL "kill '~a' # ~a " pid command))
+
+(define-extractor kill-kill9
+    ("^kill ([0-9]+)" (pid))
+  (format NIL "kill -9 '~a'" pid))
+
+(defun extract (line &optional (extractors (reverse *extractors*)))
   (loop
     :for fn :in extractors
     :for command = (funcall fn line)
