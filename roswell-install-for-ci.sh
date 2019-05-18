cb84ceffb07f6b55f5f44e246da282292b3088e6
Matteo Landi <matteo@matteolandi.net>
2019-05-18 15:12:06 +0200

    Daje

--- roswell-install-for-ci.sh
+++ roswell-install-for-ci.sh
@@ -1,14 +1,11 @@
 #!/usr/bin/env sh
 
-if [ "$TRAVIS_OS_NAME" = "windows" ]; then
-  # Install: make
-  choco install make
+OS_WIN=$(uname -s | grep -e MINGW)
 
-  # Install: rowsell
+if [ -n "$OS_WIN" ]; then
   curl -L "https://ci.appveyor.com/api/projects/snmsts/roswell-en89n/artifacts/Roswell-x86_64.zip?branch=master&job=Environment%3A%20MSYS2_ARCH%3Dx86_64,%20MSYS2_BITS%3D64,%20MSYSTEM%3DMINGW64,%20METHOD%3Dcross" \
       --output /tmp/roswell.zip
   unzip -n /tmp/roswell.zip -d /tmp/ # there is a `roswell` toplevel directory already
-  export PATH="/tmp/roswell:$PATH"
 fi
 
 # Run roswell's CI script, and since it will find `ros` already available
